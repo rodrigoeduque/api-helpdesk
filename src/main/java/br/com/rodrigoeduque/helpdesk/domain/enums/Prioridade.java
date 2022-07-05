56881b0a17db6fab4b93d223c0356fe821903691
Rodrigo Duque <rodrigoeduque@hotmail.com>
2022-07-04 21:27:37 -0300

    Criado Enums/Projeto

---
+++ src/main/java/br/com/rodrigoeduque/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,38 @@
+package br.com.rodrigoeduque.helpdesk.domain.enums;
+
+public enum Prioridade {
+
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+    private Integer codigo;
+    private String descricao;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    private Prioridade(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    private static Prioridade toEnum(Integer codigo) {
+        if (codigo == null) {
+            return null;
+        }
+
+        for (Prioridade prioridade : Prioridade.values()) {
+            if (codigo.equals(prioridade.getCodigo())) {
+                return prioridade;
+            }
+
+        }
+        throw new IllegalArgumentException("Tipo Prioridade Inválido/Inexistente");
+    }
+}
---
+++ src/main/java/br/com/rodrigoeduque/helpdesk/domain/enums/Status.java
@@ -0,0 +1,37 @@
+package br.com.rodrigoeduque.helpdesk.domain.enums;
+
+public enum Status {
+    ABERTO(0, "ABERTO"),
+    ANDAMENTO(1, "EM_ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    private static Status toEnum(Integer codigo) {
+        if (codigo == null) {
+            return null;
+        }
+
+        for (Status status : Status.values()
+        ) {
+            if (codigo.equals(status.getCodigo())) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Tipo Status Inválido/Inexistente");
+    }
+}
