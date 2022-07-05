a0e484b58f1c34d917aca7cf88bfe21212c10807
Rodrigo Duque <rodrigoeduque@hotmail.com>
2022-07-05 08:13:19 -0300

    configurado JPA para classes dominio

--- src/main/java/br/com/rodrigoeduque/helpdesk/domain/Tecnico.java
+++ src/main/java/br/com/rodrigoeduque/helpdesk/domain/Tecnico.java
@@ -1,17 +1,28 @@
 package br.com.rodrigoeduque.helpdesk.domain;
 
+import br.com.rodrigoeduque.helpdesk.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
 
+
+    private static final long serialVersionUID = 5245533118693167947L;
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
     public List<Chamado> getChamados() {
